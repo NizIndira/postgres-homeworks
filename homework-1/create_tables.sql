9d43a2efbf505cca580aa2c2f493943e00c5cbf9
Indira <nizamutdinovaindira@gmail.com>
2023-08-23 13:07:54 +0300

    tables created

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,27 @@
 -- SQL-команды для создания таблиц
+
+CREATE TABLE employees
+(
+    employee_id int PRIMARY KEY,
+    first_name varchar(50) NOT NULL,
+	last_name varchar(50) NOT NULL,
+	title text NOT NULL,
+	birth_date date NOT NULL,
+	notes text
+);
+
+CREATE TABLE customers
+(
+    customer_id varchar(50) NOT NULL,
+	company_name varchar(50) NOT NULL,
+	contact_name varchar(50) NOT NULL
+);
+
+CREATE TABLE orders
+(
+    order_id int PRIMARY KEY,
+    customer_id varchar(50) NOT NULL,
+	employee_id int NOT NULL,
+	order_date date NOT NULL,
+	ship_city varchar(50) NOT NULL
+)
